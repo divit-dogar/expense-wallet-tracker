bc0e93252d7784f99f8bd4a74d8533f11989c25b
Dayanand <divitdogar@gmail.com>
2026-08-26 13:07:47 +0530

    Add user model

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import create_async_engine
 from app.core.database import Base
 from app.core.config import settings
-
+from app.models.user import User
 
 config = context.config
 
